8c2c5613a31b69ef8f4cee00a0109254a17e1778
mikepitre <michaelepitre@gmail.com>
2015-10-26 14:42:47 -0500

    created notification when heart or food dragged and dropped onto monster, fixed bug with audio files in assests with same name as pics

--- PetRock/DragImg.swift
+++ PetRock/DragImg.swift
@@ -12,6 +12,8 @@ import UIKit
 class DragImg: UIImageView {
     
     var originalPosition: CGPoint!
+    var dropTarget: UIView?
+    
     
     
     override init(frame: CGRect) {
@@ -34,6 +36,17 @@ class DragImg: UIImageView {
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first, let target = dropTarget {
+            
+            let position = touch.locationInView(self.superview)
+            
+            if CGRectContainsPoint(target.frame, position) {
+                
+                NSNotificationCenter.defaultCenter().postNotificationName("onTargetDropped", object: nil)
+                
+            }
+        }
+        
         self.center = originalPosition
     }
     
--- PetRock/ViewController.swift
+++ PetRock/ViewController.swift
@@ -14,16 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var foodImg: DragImg!
     @IBOutlet weak var heartImg: DragImg!
     
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        foodImg.dropTarget = monsterImg
+        heartImg.dropTarget = monsterImg
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "itemDroppedOnCharacter:", name: "onTargetDropped", object: nil)
         
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
-
+    func itemDroppedOnCharacter(notif: AnyObject) {
+        print("Item dropped on character.")
+    }
 
 }
 
